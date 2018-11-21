1bda9dd23958a0f7b3e252d284a44fe64607e098
Jesse Gay <jesse@teamtreehouse.com>
2018-11-20 19:08:34 -0800

    Game Manager construction started
    Mostly copied from unit2, with new names assigned as needed.
    
    I'm stuck on the isCorrect func. Need to decide what kind of argument it takes. Figure out how to populate the labels first, then it should become clear.
    Might need to make up/down button stack views before that so the stack is proper before I start assigning values.

--- Unit-03-BoutTime-JesseGay/Unit-03-BoutTime-JesseGay/GameManager.swift
+++ Unit-03-BoutTime-JesseGay/Unit-03-BoutTime-JesseGay/GameManager.swift
@@ -6,4 +6,51 @@
 //  Copyright © 2018 Jesse Gay. All rights reserved.
 //
 
+// "Round" in unit 2 is now "Game". "Question" in unit 2 is now "Round"
+
+import GameKit
+
 import Foundation
+
+class GameManager {
+    
+    // MARK: Properties
+    let roundsPerGame = 6
+    var roundsPlayed = 0
+    var correctResponses = 0
+    var indexOfSelectedEvent = 0 // might need better name
+    var alreadyUsedInRound: [Int]   = [] // use indices
+    let eventStruct = EventStruct () // This includes the array of all events which will be used to populate the labels
+    
+    
+    
+    // MARK: Methods
+    
+    // getRandomEvent
+    func getRandomEvent() -> Event {
+        // Get random number. This is an intermediate var (which is checked for repetition within a given round before being assigned to indexOfSelectedEvent)
+        var randomNumber = GKRandomSource.sharedRandom().nextInt(upperBound: eventStruct.eventCollection.count)
+    
+    // Check for repetition within round
+    while alreadyUsedInRound.contains(randomNumber)  {
+    randomNumber = GKRandomSource.sharedRandom().nextInt(upperBound: eventStruct.eventCollection.count)
+    }
+    indexOfSelectedEvent = randomNumber
+    // Add to alreadyAskedQuestions
+    alreadyUsedInRound.append(indexOfSelectedEvent)
+    // return the question
+    return eventStruct.eventCollection[indexOfSelectedEvent]
+    }
+    
+    // Check final order
+    //func isCorrect(//what goes here?)) -> Bool {
+    
+    // if historicalEveent1.date > historicalEvent2.date etc. 
+    
+    
+    
+    
+    
+    
+    
+}
